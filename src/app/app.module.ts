ee795b548e7fbf38709dbc87ae1db1d88dd120d9
nina <nina.kasimova@gmail.com>
2022-05-25 19:39:23 +0100

    delete table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksTableComponent } from './books-table/books-table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BooksTableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
