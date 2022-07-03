62d175d6b710aa34953e626ee7b18662b03c67ca
nina <nina.kasimova@gmail.com>
2022-07-03 18:45:06 +0100

    add a server and get data from there

--- src/app/book-table/book-table.component.ts
+++ src/app/book-table/book-table.component.ts
@@ -3,6 +3,7 @@ import { Book } from '../book';
 import { Router} from "@angular/router";
 import { BookService } from '../book.service';
 
+
 @Component({
   selector: 'book-table',
   templateUrl: './book-table.component.html',
@@ -29,25 +30,33 @@ export class BookTableComponent implements OnInit {
   ];
 
   columnDefs = [
-    {headerName: 'Make', field: 'make'},
-    {headerName: 'Model', field: 'model'},
-    {headerName: 'Price', field: 'price'}
+    {headerName: 'Make', field: 'id'},
+    {headerName: 'title', field: 'book_title'},
+    {headerName: 'author', field: 'author'},
+    {headerName: 'rating', field: 'avg_rating'},
+    {headerName: 'Reviews', field: 'review_count'}
   ];
-  rowData = [
-    {make: 'Toyota', model: 'Celica', price: 35000},
-    {make: 'Ford', model: 'Mondeo', price: 32000},
-    {make: 'Porsche', model: 'Boxter', price: 72000}
-  ];
+  // rowData = [
+  //   {make: 'Toyota', model: 'Celica', price: 35000},
+  //   {make: 'Ford', model: 'Mondeo', price: 32000},
+  //   {make: 'Porsche', model: 'Boxter', price: 72000}
+  // ];
+  rowData = [];
 
   attributes = ['id', 'title', 'author', 'genre', 'rating'];
 
   ngOnInit(): void {
     this.getBooks();
+    fetch('http://localhost:3000/books')
+      .then(result => result.json())
+      .then(rowData => this.rowData = rowData);
+    console.log(typeof this.rowData);
+    //this.rowData = shortData;
   }
 
   getBooks(): void {
     this.bookService.getBooks()
-      .subscribe(books => this.books = books);
+      .subscribe(books => this.books = books)
   }
 
   onClick(book: Book) {
@@ -55,9 +64,3 @@ export class BookTableComponent implements OnInit {
     this.router.navigate(['/details/', book.id]);//then(r => console.log(r));
   }
 }
-
-
-
-
-
-
