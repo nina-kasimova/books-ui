96f9e72c28f00ce6697790d1098a4a97507378ea
nina <nina.kasimova@gmail.com>
2022-08-16 16:03:55 +0100

    get book by id from service

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Book } from "../book";
 import { BookService} from "../book.service";
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-book-details',
@@ -15,17 +15,16 @@ export class BookDetailsComponent implements OnInit {
 
   constructor(
     private bookService: BookService,
-    private route: ActivatedRoute,
-    private location: Location
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    //this.getBook();
+    this.getBook();
   }
 
-  // getBook(): void {
-  //   const id = Number(this.route.snapshot.paramMap.get('id'));
-  //   this.bookService.getBook(id)
-  //     .subscribe(book => this.book = book);
-  //   }
+  getBook(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookService.getBook(id)
+      .subscribe(book => this.book = book);
+    }
 }
--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {catchError, Observable, of, tap} from "rxjs";
-import booksData from './books_info.json';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Book } from "./book";
 
 @Injectable({
   providedIn: 'root'
@@ -18,25 +18,9 @@ export class BookService {
     return  this.http.get('http://localhost:3000/books');
   }
 
-  // getBook(id: number): Observable<Book> {
-  //   // const book = of(BOOKS.find(book => book.id === id)).pipe(
-  //   //   catchError(this.handleError<Book>(`getBook id=${id}`))
-  //   // );
-  //   const book = this.temp_books.find(book => book.id === id)!;
-  //   return of(book);
-  // }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      console.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+  getBook(id: number): Observable<Book> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Book>(url);
   }
+
 }
